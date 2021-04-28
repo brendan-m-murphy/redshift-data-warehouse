e3b754a99a9d84e6c14d57b8a39707b38e080101
Brendan Murphy <git@mellotron.anonaddy.com>
2021-04-28 14:40:38 +0100

    Created 'create_cfg.py' to set up dwh.cfg

---
+++ create_cfg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Write the credentials from a csv downloaded from AWS to a config file.
+"""
+import configparser
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
